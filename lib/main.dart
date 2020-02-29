97d7a77d3ec8f56282c452e11b56b61f939381f7
Hentioe <me@bluerain.io>
2020-02-29 10:59:12 +0800

    Init

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mikack Mobile',
+      theme: ThemeData(
+        // This is the theme
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Mikack'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Hello Mikack!"),
+      ),
+    );
+  }
+}
